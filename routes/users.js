bbbcfcbcee95dd0ccc6ad5304ec4cc52787aa595
EisenEgan <mrpatrickegan@gmail.com>
2017-05-20 18:28:58 -0500

    Second commit, end of third video

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
 
-router.get('/register', (req, res, next) => {
-  res.send('REGISTER')
+router.post('/register', (req, res, next) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  })
+  User.addUser(newUser, (err, user) => {
+    if(err) {
+      res.json({success: false, msg: 'Failed to register user'})
+    } else {
+      res.json({success: true, msg: 'User registered'})
+    }
+  })
 })
 
 router.post('/authenticate', (req, res, next) => {
@@ -11,10 +26,6 @@ router.post('/authenticate', (req, res, next) => {
 
 router.get('/profile', (req, res, next) => {
   res.send('PROFILE')
-})
-
-router.get('/validate', (req, res, next) => {
-  res.send('VALIDATE')
 })
 
 module.exports = router
